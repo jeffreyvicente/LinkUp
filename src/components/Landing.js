6c620e9f143f87deb87fa63c36c4db36d07a1303
SethSchnrt <Seth.Schoenert@gmail.com>
2023-08-25 18:52:13 -0500

    Finished AuthModal component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import logo from '../logo.png';
+import AuthModal from './AuthModal';
 
 function LoginLanding() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleOpenModal = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
     return (
         <Container fluid className="login-landing">
             <div className="logo-section">
@@ -11,8 +22,10 @@ function LoginLanding() {
                 <h1 className='landing-name'>LinkUp</h1>
             </div>
             <p className="p-landing">To continue further,</p>
-            <Button variant="primary" className='landing-button'>Login or Sign Up</Button>
+            <Button onClick={handleOpenModal} variant="primary" className='landing-button'>Login or Sign Up</Button>
+            <AuthModal show={showModal} handleClose={handleCloseModal} />
         </Container>
+        
     );
 }
 
