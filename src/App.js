56272a452aead3a4611869a44687fc5e5198b9ec
SethSchnrt <Seth.Schoenert@gmail.com>
2023-08-24 20:22:47 -0500

    Home page, React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navbar';
-import LoginLanding from './components/Landing';
+ import LoginLanding from './components/Landing'; 
+import Home from './pages/Home';
+import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
  
   return (
+    <Router>
     <div>
       <Navigation />
-      <LoginLanding />
+    {/*  <LoginLanding /> */}
+     <Home />
     </div>
+    </Router>
   );
 }
 
