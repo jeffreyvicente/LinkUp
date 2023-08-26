c2bb88359b497f94b3e84bbe8a6fcd8c8eb7cd9d
Jeffrey V <jeffreyvicente@hotmail.com>
2023-08-26 16:56:57 -0700

    Added logic to pull from the database in eventjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navbar';
-<<<<<<< Updated upstream
-import LoginLanding from './components/Landing';
-=======
  import LoginLanding from './components/Landing'; 
 import Home from './pages/Home';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -11,20 +8,15 @@ import Checkout from './pages/Test';
 import Events from './pages/Events';
 
 
->>>>>>> Stashed changes
 function App() {
  
   return (
     <div>
       <Navigation />
-<<<<<<< Updated upstream
-      <LoginLanding />
-=======
     {/* <LoginLanding /> */}
     {/*  <Home /> */}
     {/*<Events /> */}
     <Checkout />
->>>>>>> Stashed changes
     </div>
   );
 }
