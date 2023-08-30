4c226c876bd2f920bb78385c363be5c4ce35b20e
SethSchnrt <Seth.Schoenert@gmail.com>
2023-08-30 14:42:36 -0500

    Signup working, Route to home when login working, Route to landing when not logged in working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navbar';
 import LoginLanding from './components/Landing'; 
