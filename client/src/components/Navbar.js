8f42bac0c8703956fd2887ce6540124ec27010af
David Gaspard <dgaspard7777@gmail.com>
2023-08-31 20:57:51 -0500

    back to href

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -56,7 +56,7 @@ function Navigation() {
             {
                 user ? (
                     <>
-                    <Nav.Link to={`/profiles/${user.username}`} className="NavLink Nav-Username">{user.username} <img src={personCircle} alt="User" width="20" height="16" className='invert-icon'/> </Nav.Link>
+                    <a href={`/profiles/${user.username}`} className="NavLink Nav-Username">{user.username} <img src={personCircle} alt="User" width="20" height="16" className='invert-icon'/> </a>
                     <Nav.Link onClick={handleUserLogout} className="NavLink">Logout <img src={logoutIcon} alt="User" width="20" height="16" className='invert-icon'/></Nav.Link>
                     </>
                 ) : ( 
